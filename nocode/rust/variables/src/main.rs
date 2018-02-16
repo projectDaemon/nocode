b71861e09c22825d1c886e5d7e20302263ee03ef
everettjf <everettjf@live.com>
2018-02-16 22:02:11 +0800

    variables

---
+++ nocode/rust/variables/src/main.rs
@@ -0,0 +1,28 @@
+
+
+
+const MAX_POINTS: u32 = 100_000;
+const MAX_POINTS1: u32 = 100000;
+
+
+fn main() {
+    println!("The contants is : {}",MAX_POINTS);
+    println!("The contants is : {}",MAX_POINTS1);
+
+    let mut x = 5;
+    println!("The value of x is : {}",x);
+    x = 6;
+    println!("The value of x is : {}",x);
+
+
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
+    
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("The length of spaces is: {}", spaces);
+}
+
