650fcea9e71fa06c90f91ca81b56586fa45cc39b
everettjf <everettjf@live.com>
2018-02-21 22:44:35 +0800

    results

---
+++ nocode/rust/results/src/main.rs
@@ -0,0 +1,23 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let f = File::open("hello.txt");
+    
+    let f = match f{
+        Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.txt"){
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!("Error create : {:?}",e);
+                }
+            }
+        },
+        Err(error) => {
+            panic!("Error info : {:?}",error);
+        },
+    };
+
+    println!("Hello, world!,{:?}",f);
+}
