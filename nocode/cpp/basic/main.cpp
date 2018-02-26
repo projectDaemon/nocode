edbb84755408b3beff060847f5e91218339f5849
everettjf <everettjf@live.com>
2018-02-26 23:15:45 +0800

    singleton

--- nocode/cpp/basic/main.cpp
+++ nocode/cpp/basic/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
+
+
+template <class... T>
+void f(T... args){
+    cout << sizeof...(args)<<endl;
+}
+
+
+template <typename T>
+void print(T t){
+    cout << "last "<< t<<endl;
+}
+
+template <typename T,typename...Args>
+void print(T head,Args... rest){
+    cout << "param "<<head<<endl;
+    print(rest...);
+};
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    f(1,2,3,4);
+
+    print(1,2,3,4);
     return 0;
 }
--- nocode/cpp/singleton/main.cpp
+++ nocode/cpp/singleton/main.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
 
+
+template <typename T>
+class Singleton{
+public:
+    template<typename... Args>
+    static T* Instance(Args&&... args){
+        if(inst_ == nullptr){
+            inst_ = new T(std::forward<Args>(args)...);
+        }
+        return inst_;
+    }
+
+    static T *GetInstance(){
+        if(inst_ == nullptr){
+            return nullptr;
+        }
+        return inst_;
+    }
+
+    static void Destroy(){
+        if(inst_ == nullptr)
+            return;
+        delete inst_;
+        inst_ = nullptr;
+    }
+private:
+    static T *inst_;
+};
+template <typename T>
+T* Singleton<T>::inst_ = nullptr;
+
+
+//////
+
+
+struct A{
+    A(const std::string&){std::cout << "lvalue"<<std::endl;}
+    A(std::string&&){std::cout << "rvalue"<<std::endl;}
+};
+
+struct B{
+    B(const std::string&){std::cout << "lvalue"<<std::endl;}
+    B(std::string&&){std::cout << "rvalue"<<std::endl;}
+};
+
+
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string str = "hello";
+    Singleton<A>::Instance(str);
+    Singleton<B>::Instance(std::move(str));
+
+    Singleton<A>::Destroy();
+    Singleton<B>::Destroy();
     return 0;
 }
